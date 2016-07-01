0493a7f8b60ae5a1d45ff92096ce54342f49330b
TheTrishMapow <trishmapow@users.noreply.github.com>
2016-07-01 20:47:52 +1000

    Basic transmit script
    Test if module is working

---
+++ Transmit.ino
@@ -0,0 +1,16 @@
+#define rf 12
+#define led 13
+
+void setup() {
+  pinMode(rf, OUTPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(rf, HIGH);
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(rf, LOW);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
